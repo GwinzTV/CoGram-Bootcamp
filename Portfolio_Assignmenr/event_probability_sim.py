4aba39c6d7da15b970881d4c0558692ad8f25e4e
Josh Iyinkanmi <89524882+GwinzTV@users.noreply.github.com>
2024-03-27 21:55:31 +0000

    addded error handling to task 6

---
+++ Portfolio_Assignmenr/event_probability_sim.py
@@ -0,0 +1,7 @@
+# includes go here
+
+print("Welcome to the Event Probability Simulator!")
+
+name = input("Please enter your name: ")
+
+print()
--- Task6/holiday.py
+++ Task6/holiday.py
@@ -1,30 +1,50 @@
-# User input:
-city_flight = input("which city are you flying to?  ")
-num_nights = int(input("How many nights are you staying at the hotel? "))
-rental_days = int(input("What is the duration in days of your holiday? "))
 
 # Function definitions:
-def hotel_cost(nights = num_nights):
-    total_cost = num_nights*89
+def hotel_cost(nights):
+    total_cost = nights*89
     return total_cost
 
-def plane_cost(flight = city_flight):
-    if city_flight == 'NYC':
+def plane_cost(flight):
+    if flight == 'new york':
         total_cost = 240
-    elif city_flight == 'LDN':
+    elif flight == 'london':
         total_cost = 100
-    elif city_flight == 'SPN':
+    elif flight == 'spain':
         total_cost = 50
     else:
         total_cost = 89
     return total_cost
 
-def car_rental(days = rental_days):
+def car_rental(days):
     total_cost = days*50
     return total_cost
 
 def holiday_cost(hotel, car, plane):
     print(f'The total sum of the holiday, including: hotel costs (£{hotel}), car rental (£{car}), and plane costs (£{plane}), is £{hotel+car+plane}.')
 
+# User input:
+try:
+    city_flight = input("which city are you flying to?  ").lower()
+    num_nights = int(input("How many nights are you staying at the hotel? "))
+    rental_days = int(input("What is the duration in days of your holiday? "))
+
+    if num_nights < 0 or rental_days < 0:
+        loop = True
+        while loop:
+            print("\n\nNumber of nights and rental days cannot be negative.\n")
+            num_nights = int(input("How many nights are you staying at the hotel? "))
+            rental_days = int(input("What is the duration in days of your holiday? "))
+            if num_nights > 0 or rental_days > 0:
+                loop = False
+
+    car = car_rental(rental_days)
+    flight = plane_cost(city_flight)
+    hotel = hotel_cost(num_nights)
+
+except ValueError as ve:
+    print(f"Error: {ve}")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+
 # script:
-holiday_cost(hotel_cost(), car_rental(), plane_cost())
+holiday_cost(hotel, car, flight)
