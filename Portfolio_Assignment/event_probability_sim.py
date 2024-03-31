a6d9beeceb00c991edb5b07137dbebc57911f1a4
Josh Iyinkanmi <89524882+GwinzTV@users.noreply.github.com>
2024-03-31 20:29:06 +0100

    adding error handling

--- Portfolio_Assignment/event_probability_sim.py
+++ Portfolio_Assignment/event_probability_sim.py
@@ -4,36 +4,47 @@ import math as m
 class ProbabilitySimulator:
     def __init__(self):
         self.events = {}
+        self.menu_text = "\nDo you wish to continue?\n\nPress:\n[1] - Main menu\n[2] - Exit\n\nEnter Your Selection: "
 
     def permutations(self):
-        n = int(input("Please enter the number of items to choose from: "))         #EH
-        r = int(input("Please enter the number of items chosen to be arranged: "))  #EH
-        print(f"The permutation is: {m.perm(n,r)}\nThe probability of a permutation is: {100/m.perm(n,r): .4f}")
+        n = int(input("Please enter the number of items to choose from: "))
+        r = int(input("Please enter the number of items chosen to be arranged: "))
+        try:
+            print(f"The permutation is: {m.perm(n,r)}\nThe probability of a permutation is: {100/m.perm(n,r): .4f}")
+        except ValueError:
+            print("Hey, the values entered must be non-negative integers")
 
     def combinations(self):
-        n = int(input("Please enter the number of items to choose from: "))         #EH
-        r = int(input("Please enter the number of items chosen to be chosen: "))    #EH
-        print(f"The combination is: {m.comb(n,r)}\nThe probability of a combination is: {100/m.comb(n,r): .2f}")
+        n = int(input("Please enter the number of items to choose from: "))
+        r = int(input("Please enter the number of items chosen to be chosen: "))
+        try:
+            print(f"The combination is: {m.comb(n,r)}\nThe probability of a combination is: {100/m.comb(n,r): .2f}")
+        except ValueError:
+            print("Hey, the values entered must be non-negative integers")
 
-    def add_event(self, name):  # adds event and probability to the simulator
-        ask_again = True
-        while True:
-            if ask_again:
-                event = input(f"{name} please enter your event:\n")
-                probability = float(input("Also, please enter the probability of this event:\n")) #EH
-                self.events[event] = probability
-                ask = input("Do you want to add anymore events? [Y/N]\n").lower()   #EH
-                if ask == 'n':
-                    ask_again = False
-            else:
-                break
+    # def add_event(self, name):  # adds event and probability to the simulator
+    #     ask_again = True
+    #     while True:
+    #         if ask_again:
+    #             event = input(f"{name} please enter your event:\n")
+    #             probability = float(input("Also, please enter the probability of this event:\n")) #EH
+    #             self.events[event] = probability
+    #             ask = input("Do you want to add anymore events? [Y/N]\n").lower()   #EH
+    #             if ask == 'n':
+    #                 ask_again = False
+    #         else:
+    #             break
 
     def trials_sim(self, name):
         event = input(f'Hey {name}! Please enter the event you want to simulate:\n E.g. Drawing a king from a deck of cards\nEvent: ')
         probability_event = float(input(f'Please enter the probability of {event} happening: '))
+        if not 0 < probability_event < 1:
+            raise Exception(f"\nSorry {name}, you must enter a probability of event as a decimal between 0 - 1\n")
         trial = int(input("Please enter the number of trials: ")) #EH
+
         success = np.random.rand(trial) < probability_event
         success_count = np.sum(success)
+        print("Value entered for number of trial must be non-negative")
         result = success_count / trial
         print(f"The probability of {event} happening, given {trial} trials is: {result*100}%")
 
@@ -54,20 +65,26 @@ class ProbabilitySimulator:
     def run_simulation(self):
         print("Welcome to the Event Probability Simulator!")
         name = input("Please enter your name: ")
-        print(f"Hi {name}! Please select one of the following operations:\n")
-        print("[1] - Calculate Permutation\n[2] - Calculate Combinations")
-        print("[3] - Simulate an event given a number of trials\n[4] - Update probability using Bayes' Theorem\n")
-        selection = int(input(f'{name} enter your selection: ')) #EH
+        while True:
+            print(f"Hi {name}! Please select one of the following operations:\n")
+            print("[1] - Calculate Permutation\n[2] - Calculate Combinations")
+            print("[3] - Simulate an event given a number of trials\n[4] - Update probability using Bayes' Theorem\n")
+            selection = int(input(f'{name} enter your selection: ')) #EH
 
-        # navigates to the users selected operation
-        if selection == 1:
-            self.permutations()
-        elif selection == 2:
-            self.combinations()
-        elif selection == 3:
-            self.trials_sim(name)
-        elif selection == 4:
-            self.bayes_theorem()
+            # navigates to the users selected operation
+            if selection == 1:
+                self.permutations()
+            elif selection == 2:
+                self.combinations()
+            elif selection == 3:
+                self.trials_sim(name)
+            elif selection == 4:
+                self.bayes_theorem()
+            
+            menu_exit = int(input(self.menu_text))
+            if menu_exit == 2:
+                print("\nYou have reached the End of the simulation, Cheers!\n")
+                break
 
 
 # Script:
