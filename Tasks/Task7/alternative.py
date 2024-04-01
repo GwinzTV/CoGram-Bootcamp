d68ee1851a9a0934250ebdc29e90b6a2099cbfb2
Josh Iyinkanmi <89524882+GwinzTV@users.noreply.github.com>
2024-04-01 21:34:09 +0100

    started alternative script

---
+++ Tasks/Task7/alternative.py
@@ -0,0 +1,9 @@
+
+class alternate:
+    def __init__(self):
+        pass
+
+    def user_input(self):
+        string = input("Hi user!, Please enter your sentence:\n")
+        return string
+    
