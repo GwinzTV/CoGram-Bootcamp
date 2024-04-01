9ace08f45264ea4baeb85fdcea2331af89aed933
Josh Iyinkanmi <89524882+GwinzTV@users.noreply.github.com>
2024-04-01 22:10:51 +0100

    implemented all functions

--- Tasks/Task7/alternative.py
+++ Tasks/Task7/alternative.py
@@ -1,9 +1,39 @@
 
 class alternate:
     def __init__(self):
-        pass
-
-    def user_input(self):
-        string = input("Hi user!, Please enter your sentence:\n")
-        return string
+        self.result = ''
+        self.user_input = input("Hi user!, Please enter your sentence:\n")
     
+    def characters(self):
+        result = []
+        string = self.user_input
+        for i in range(len(string)):
+            if i % 2 == 0:
+                result.append(string[i].upper())
+            else:
+                result.append(string[i].lower())
+        self.result = ''.join(result)
+        print('')
+        print('Alternating characters:')
+        print(self.result)
+        print('')
+
+
+    def words(self):
+        result = []
+        string = self.user_input.split()
+        for i in range(len(string)):
+            if i % 2 == 0:
+                result.append(string[i].upper())
+            else:
+                result.append(string[i].lower())
+        self.result = ' '.join(result)
+        print('')
+        print('Alternating words:')
+        print(self.result)
+        print('')
+
+# Script:
+alt = alternate()
+alt.characters()
+alt.words()
