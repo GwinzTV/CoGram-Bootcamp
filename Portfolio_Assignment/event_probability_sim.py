41f57ac3248d9891f8d9d24fbe5d2d6325224097
Josh Iyinkanmi <89524882+GwinzTV@users.noreply.github.com>
2024-04-01 01:40:19 +0100

    finished implementation

--- Portfolio_Assignment/event_probability_sim.py
+++ Portfolio_Assignment/event_probability_sim.py
@@ -3,14 +3,13 @@ import math as m
 
 class ProbabilitySimulator:
     def __init__(self):
-        self.events = {}
         self.menu_text = "\nDo you wish to continue?\n\nPress:\n[1] - Main menu\n[2] - Exit\n\nEnter Your Selection: "
 
     def permutations(self):
         n = int(input("Please enter the number of items to choose from: "))
         r = int(input("Please enter the number of items chosen to be arranged: "))
         try:
-            print(f"The permutation is: {m.perm(n,r)}\nThe probability of a permutation is: {100/m.perm(n,r): .4f}")
+            print(f"The permutation is: {m.perm(n,r)}\nThe probability of a permutation is: {100/m.perm(n,r): .4f}%")
         except ValueError:
             print("Hey, the values entered must be non-negative integers")
 
@@ -18,7 +17,7 @@ class ProbabilitySimulator:
         n = int(input("Please enter the number of items to choose from: "))
         r = int(input("Please enter the number of items chosen to be chosen: "))
         try:
-            print(f"The combination is: {m.comb(n,r)}\nThe probability of a combination is: {100/m.comb(n,r): .2f}")
+            print(f"The combination is: {m.comb(n,r)}\nThe probability of a combination is: {100/m.comb(n,r): .2f}%")
         except ValueError:
             print("Hey, the values entered must be non-negative integers")
 
@@ -39,16 +38,20 @@ class ProbabilitySimulator:
 
     def bayes_theorem(self):
         text = "Please enter the scenario you want to compute\nIn the format: 'event A' given 'event B'\nE.g. stock price going 5%, given the CEO of the company is fired\n**It is important that the 'given' keyword is specified!"
-        scenario = input(f"{text}\n\nScenario: ").split('given')
-        event_a, event_b = scenario[0].strip(), scenario[1].strip()
-        b_given_a = float(input(f"Please enter the probability of {event_b}, given {event_a} has happened:\n"))  #EH
-        a = float(input(f"Please enter the probability of {event_a} happening:\n"))        #EH
-        b = float(input(f"Please enter the probability of {event_b} happening:\n"))        #EH
+        scenario = input(f"{text}\n\nScenario: ")
+        scenario_list = scenario.split('given')
+        if 'given' not in scenario:
+            raise Exception("\nScenario specified in wrong format, you are missing the 'given' keyword.\n")
+        event_a, event_b = scenario_list[0].strip(), scenario_list[1].strip()
+        b_given_a = float(input(f"Please enter the probability of {event_b}, given {event_a} has happened:\n"))
+        a = float(input(f"Please enter the probability of {event_a} happening:\n"))
+        b = float(input(f"Please enter the probability of {event_b} happening:\n"))
+        if not 0<a<1 or not 0<b<1 or not 0<b_given_a<1:
+            raise Exception(f"\nSorry, you must enter a probability of event as a decimal between 0 - 1\n")
         denominator = b
         numerator = b_given_a * a
         a_given_b = numerator / denominator
-        print(f'The probability of {''.join(scenario)}, is {a_given_b*100: .2f}%')
-
+        print(f'The probability of {scenario}, is {a_given_b*100: .2f}%')
     
     # facilitates the simulation of the scenarios and returns the probabilities
     def run_simulation(self):
@@ -58,7 +61,9 @@ class ProbabilitySimulator:
             print(f"Hi {name}! Please select one of the following operations:\n")
             print("[1] - Calculate Permutation\n[2] - Calculate Combinations")
             print("[3] - Simulate an event given a number of trials\n[4] - Update probability using Bayes' Theorem\n")
-            selection = int(input(f'{name} enter your selection: ')) #EH
+            selection = 0
+            while selection not in [1,2,3,4]:
+                selection = int(input(f'{name} enter your selection: '))
 
             # navigates to the users selected operation
             if selection == 1:
@@ -79,21 +84,3 @@ class ProbabilitySimulator:
 # Script:
 simulator = ProbabilitySimulator()
 simulator.run_simulation()
-
-    
-    
-    
-    
-    
-    # def add_event(self, name):  # adds event and probability to the simulator
-    #     ask_again = True
-    #     while True:
-    #         if ask_again:
-    #             event = input(f"{name} please enter your event:\n")
-    #             probability = float(input("Also, please enter the probability of this event:\n")) #EH
-    #             self.events[event] = probability
-    #             ask = input("Do you want to add anymore events? [Y/N]\n").lower()   #EH
-    #             if ask == 'n':
-    #                 ask_again = False
-    #         else:
-    #             break
