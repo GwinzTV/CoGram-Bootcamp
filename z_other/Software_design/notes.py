a60d47a6d338c6acb69e124cbfeb80d540233c18
Josh Iyinkanmi <89524882+GwinzTV@users.noreply.github.com>
2024-04-28 22:53:18 +0100

    sequence diagrams

---
+++ z_other/Software_design/notes.py
@@ -0,0 +1,40 @@
+'''
+How does modularisation contribute to the scalability of a software system?
+
+ - By enabling independent deployment of modules. So they can work at scale, i.e. on multiple computers
+
+In a sequence diagram, what does an arrow represent?
+
+ - Control flow
+
+What is the purpose of a use case diagram?
+
+ - It represents the functional requirements of a system from the perspective of its users.
+'''
+
+'''
+Modularisation:
+ - Breaking down software into independent modules enhances maintainability, scalability, and reusability.
+
+Sequence Diagrams:
+ - Visualising components interactions elucidates system behaviour over time, aiding in comprehension
+    and optimisation.
+
+Use Case Diagrams:
+ - Representing system functionalities from the user viewpoints aid in requirement analysis and 
+    stakeholder communication.
+
+Modularisation Importance:
+ - It streamlines development, promotes code reusability, and enables easier collaboration among teams.
+
+Sequence Diagrams' Utility:
+ - They highlight the sequence of events, showcasing message exchanges and system flow.
+
+User Case Diagrams' Significance:
+ - They provide a holistic view of system functionalities, ensuring alignment with user needs
+    and expectations.
+
+Overall Importance:
+ - Together, these tools facilitate the design, development, and understanding of complex softare systems,
+    ensuring efficiency and user satisfaction.
+'''
