c9a573a21cf8d6a7d596439a9276caef0406550a
Josh Iyinkanmi <89524882+GwinzTV@users.noreply.github.com>
2024-05-29 01:00:06 +0100

    txt_update1

--- Tasks/Task22/SQLQuery.py
+++ Tasks/Task22/SQLQuery.py
@@ -14,15 +14,17 @@ create_table = '''CREATE TABLE IF NOT EXISTS Student(
     PROJ_NUM INT(2)
     );'''
 
-insert_values = '''INSERT INTO Student VALUES'''
-users = [
-    (01, 'Snow', 'Jon', 'E', 2014-04-05, 201, 6),
-    (02, 'Stark', 'Arya', 'C', 2017-07-12, 305, 11),
-    (03, 'Lannister', 'Jamie', 'C', 2012-09-05, 101, 2),
-    (04, 'Lannister', 'Cercei', 'J', 2012-09-05, 101, 2),
-    (05, 'Greyjoy', 'Theon', 'I', 2015-12-09, 402, 14),
-    (06, 'Tyrell', 'Margaery', 'Y', 2017-07-12, 402, 14),
-    (07, 'Baratheon', 'Tommen', 'R', 2019-06-13, 201, 5)]
+insert_values = '''INSERT INTO Student (STU_NUM, STU_SNAME, STU_FNAME,
+STU_INITIAL, STU_STARTDATE, COURSE_CODE, PROJ_NUM)
+VALUES
+    ('11', 'Snow', 'Jon', 'E', '2014-04-05', '201', 6),
+    ('12', 'Stark', 'Arya', 'C', '2017-07-12', '305', 11),
+    ('13', 'Lannister', 'Jamie', 'C', '2012-09-05', '101', 2),
+    ('14', 'Lannister', 'Cercei', 'J', '2012-09-05', '101', 2),
+    ('15', 'Greyjoy', 'Theon', 'I', '2015-12-09', '402', 14),
+    ('16', 'Tyrell', 'Margaery', 'Y', '2017-07-12', '402', 14),
+    ('17', 'Baratheon', 'Tommen', 'R', '2019-06-13', '201', 5);
+'''
 
 
 def main():
