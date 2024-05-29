48109a55df2c03a984b837a6db5bca585c021388
Josh Iyinkanmi <89524882+GwinzTV@users.noreply.github.com>
2024-05-29 01:56:59 +0100

    finished task

--- Tasks/Task22/SQLQuery.py
+++ Tasks/Task22/SQLQuery.py
@@ -17,20 +17,40 @@ create_table = '''CREATE TABLE IF NOT EXISTS Student(
 insert_values = '''INSERT INTO Student (STU_NUM, STU_SNAME, STU_FNAME,
 STU_INITIAL, STU_STARTDATE, COURSE_CODE, PROJ_NUM)
 VALUES
-    ('11', 'Snow', 'Jon', 'E', '2014-04-05', '201', 6),
-    ('12', 'Stark', 'Arya', 'C', '2017-07-12', '305', 11),
-    ('13', 'Lannister', 'Jamie', 'C', '2012-09-05', '101', 2),
-    ('14', 'Lannister', 'Cercei', 'J', '2012-09-05', '101', 2),
-    ('15', 'Greyjoy', 'Theon', 'I', '2015-12-09', '402', 14),
-    ('16', 'Tyrell', 'Margaery', 'Y', '2017-07-12', '402', 14),
-    ('17', 'Baratheon', 'Tommen', 'R', '2019-06-13', '201', 5);
+    ('01', 'Snow', 'Jon', 'E', '2014-04-05', '201', 6),
+    ('02', 'Stark', 'Arya', 'C', '2017-07-12', '305', 11),
+    ('03', 'Lannister', 'Jamie', 'C', '2012-09-05', '101', 2),
+    ('04', 'Lannister', 'Cercei', 'J', '2012-09-05', '101', 2),
+    ('05', 'Greyjoy', 'Theon', 'I', '2015-12-09', '402', 14),
+    ('06', 'Tyrell', 'Margaery', 'Y', '2017-07-12', '305', 14),
+    ('07', 'Baratheon', 'Tommen', 'R', '2019-06-13', '201', 5);
 '''
+
+select_condition = '''SELECT * FROM Student WHERE COURSE_CODE='305';'''
+
+delete = '''DELETE FROM Student
+WHERE STU_FNAME='Jamie'
+AND STU_SNAME='Lannister'
+AND STU_STARTDATE='2012-09-05'
+AND COURSE_CODE='101';'''
+
+update = '''UPDATE Student SET COURSE_CODE = '304' WHERE STU_NUM='07';'''
+
+update2 = '''UPDATE Student SET PROJ_NUM=14
+WHERE STU_STARTDATE < '2016-01-01'
+AND COURSE_CODE >= '201';'''
+
+drop = '''DROP TABLE Student'''
 
 
 def main():
     cursor.execute(create_table)
-    cursor.execute(insert_values)
+    # cursor.execute(delete)
     # cursor.execute(insert_values)
+    # result = list(cursor.execute(select_condition))
+    # for x in result:
+    #     print(x)
+    # cursor.execute(drop)
 
     db.commit()
     db.close()
